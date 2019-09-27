8c2f5fd0e5b33e7fa4e520ee8438c41f54ad221a
Lorenzo Ramponi <lorenzoramponi18@gmail.com>
2019-09-27 10:29:22 +0200

    added more paths

---
+++ src/SpaceShip.cpp
@@ -0,0 +1,18 @@
+/*
+ * SpaceShip.cpp
+ *
+ *  Created on: 27 set 2019
+ *      Author: loren
+ */
+
+#include "SpaceShip.h"
+
+SpaceShip::SpaceShip() {
+	// TODO Auto-generated constructor stub
+
+}
+
+SpaceShip::~SpaceShip() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/SpaceShip.h
@@ -0,0 +1,17 @@
+/*
+ * SpaceShip.h
+ *
+ *  Created on: 27 set 2019
+ *      Author: loren
+ */
+
+#ifndef SPACESHIP_H_
+#define SPACESHIP_H_
+
+class SpaceShip {
+public:
+	SpaceShip();
+	virtual ~SpaceShip();
+};
+
+#endif /* SPACESHIP_H_ */
