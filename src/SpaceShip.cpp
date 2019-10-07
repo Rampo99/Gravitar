77500dfe452afcdf6148486f6748f5173bbe1460
6SNAK36 <43323314+6SNAK36@users.noreply.github.com>
2019-10-07 23:02:26 +0200

    Update SpaceShip.cpp
    atm the ship can only move, shooting coming soon

--- src/SpaceShip.cpp
+++ src/SpaceShip.cpp
@@ -1,33 +1,121 @@
-#include "SpaceShip.h"
+#include <SFML/Graphics.hpp>
+#include <list>
+#include <math.h>
+#include <iostream>
+#include "Spaceship.h"
 
-SpaceShip::SpaceShip() {
-	x = 0;
-	y = 0;
-	ship.setFillColor(sf::Color(255,255,255));
-	ship.setPointCount(4);
-	ship.setPoint(0, sf::Vector2f(0,30));
-	ship.setPoint(1, sf::Vector2f(30,60));
-	ship.setPoint(2, sf::Vector2f(100,30));
-	ship.setPoint(3, sf::Vector2f(30,0));
-	ship.setPosition(683,384);
+
+#define PI 3.14159265
+
+
+using namespace std;
+using namespace sf;
+
+
+Spaceship::Spaceship()
+{
+	ship = ConvexShape(16);
+	ship.setFillColor(Color::Green);
+	ship.setPoint(0, Vector2f(0, 0));
+	ship.setPoint(1, Vector2f(7, 14));
+	ship.setPoint(2, Vector2f(0, 28));
+	ship.setPoint(3, Vector2f(7, 42));
+	ship.setPoint(4, Vector2f(0, 56));
+	ship.setPoint(5, Vector2f(14, 49));
+	ship.setPoint(6, Vector2f(28, 56));
+	ship.setPoint(7, Vector2f(42, 49));
+	ship.setPoint(8, Vector2f(56, 56));
+	ship.setPoint(9, Vector2f(49, 42));
+	ship.setPoint(10, Vector2f(56, 28));
+	ship.setPoint(11, Vector2f(49, 14));
+	ship.setPoint(12, Vector2f(56, 0));
+	ship.setPoint(13, Vector2f(42, 7));
+	ship.setPoint(14, Vector2f(28, 0));
+	ship.setPoint(15, Vector2f(14, 7));
+	ship.setOrigin(28, 28);
+	ship.setPosition(100, 100);
+	move_left = move_up = move_right = move_down = false;
+	speed = 400;
 }
 
-SpaceShip::~SpaceShip() {
+void Spaceship::direction(Event event)
+{
+	switch (event.type) {
+	case Event::KeyPressed:
+		switch (event.key.code) {
+		case Keyboard::A:
+			move_left = true;
+			if (Keyboard::isKeyPressed(Keyboard::Key::D))
+				move_right = false;
+			break;
+		case Keyboard::W:
+			move_up = true;
+			if (Keyboard::isKeyPressed(Keyboard::Key::S))
+				move_down = false;
+			break;
+		case Keyboard::D:
+			move_right = true;
+			if (Keyboard::isKeyPressed(Keyboard::Key::A))
+				move_left = false;
+			break;
+		case Keyboard::S:
+			move_down = true;
+			if (Keyboard::isKeyPressed(Keyboard::Key::W))
+				move_up = false;
+			break;
+		default:
+			break;
+		}
+		break;
+		case Event::KeyReleased:
+			switch (event.key.code) {
+			case Keyboard::A:
+				move_left = false;
+				if (Keyboard::isKeyPressed(Keyboard::Key::D))
+					move_right = true;
+				break;
+			case Keyboard::W:
+				move_up = false;
+				if (Keyboard::isKeyPressed(Keyboard::Key::S))
+					move_down = true;
+				break;
+			case Keyboard::D:
+				move_right = false;
+				if (Keyboard::isKeyPressed(Keyboard::Key::A))
+					move_left = true;
+				break;
+			case Keyboard::S:
+				move_down = false;
+				if (Keyboard::isKeyPressed(Keyboard::Key::W))
+					move_up = true;
+				break;
+			default:
+				break;
+			}
+			break;
+			default:
+				break;
+	}
 }
 
-void SpaceShip::onEvent(sf::Event& event) {
-
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-        x = 1;
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-        y = 1;
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-        x = -1;
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-        y = -1;
+void Spaceship::move()
+{
+	Time elapsed = clock.restart();
+	double coeff = 1;
+	if ((move_up || move_down) && (move_left || move_right))
+		coeff = sin(45 * PI / 180);
+	if (move_left)
+		ship.setPosition(ship.getPosition().x - elapsed.asSeconds() * speed * coeff, ship.getPosition().y);
+	if (move_up)
+		ship.setPosition(ship.getPosition().x, ship.getPosition().y - elapsed.asSeconds() * speed * coeff);
+	if (move_right)
+		ship.setPosition(ship.getPosition().x + elapsed.asSeconds() * speed * coeff, ship.getPosition().y);
+	if (move_down)
+		ship.setPosition(ship.getPosition().x, ship.getPosition().y + elapsed.asSeconds() * speed * coeff);
 }
 
-void SpaceShip::draw(sf::RenderWindow& w){
-
-	w.draw(ship);
+void  Spaceship::draw(RenderWindow& window)
+{
+	window.draw(ship);
 }
+
