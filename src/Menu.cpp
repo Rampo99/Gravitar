ea7a25624f4cad7c7e890ff4ade0fb056d14eaac
Rampo99 <lorenzoramponi18@gmail.com>
2019-10-11 12:24:24 +0200

    Fixed files

---
+++ src/Menu.cpp
@@ -0,0 +1,14 @@
+/*
+ * Menu.cpp
+ *
+ *  Created on: 11 ott 2019
+ *      Author: loren
+ */
+
+#include "Menu.h"
+
+Menu::Menu() {
+	// TODO Auto-generated constructor stub
+
+}
+
---
+++ src/Screens.h
@@ -0,0 +1,11 @@
+#ifndef SCREENS_H
+#define SCREENS_H
+
+//Basic Screen Class
+#include "Screen.h"
+
+//Including each screen of application
+#include "menu.h"
+#include "game.h"
+
+#endif
